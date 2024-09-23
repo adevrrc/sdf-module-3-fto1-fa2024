f2f2c3f57849078dabd31e68fc10fb7b8b7a081c
Damien Altenburg <daltenburg@rrc.ca>
2024-09-23 12:50:19 -0500

    Update to include examples of boolean expressions.

--- boolean_expressions.py
+++ boolean_expressions.py
@@ -4,3 +4,34 @@ Author:
 Date:
 Usage: python boolean_expressions.py
 """
+
+# Literals
+print(True)
+print(False)
+
+# Java: true, false
+
+# Variables
+is_damien_here: bool = True
+print(is_damien_here)
+print(type(is_damien_here))
+
+# Functions
+name: str = "Damien"
+
+print(name.isalpha())
+print(name.isdigit())
+
+is_alphabetic: bool = name.isalpha()
+print(is_alphabetic)
+
+# Operators
+# Comparison Operators
+age: int = 10
+
+print(age == 25)
+print(age != 25)
+print(age < 35)
+print(age <= 25)
+print(age > 56)
+print(age >= 2)
