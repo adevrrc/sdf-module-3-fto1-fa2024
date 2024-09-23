a212f332d2f764919fe436823decd670b5619163
Damien Altenburg <daltenburg@rrc.ca>
2024-09-23 14:27:28 -0500

    Updated script to include examples of if statements.

--- selections.py
+++ selections.py
@@ -5,5 +5,42 @@ Date:
 Usage: python selections.py
 """
 
-age = 25
-name = "Damien"
+age: int = 125
+name: str = "David"
+
+if age == 25:
+    print("You are at the perfect age.")
+
+print("This is not part of the if block.")
+
+if age > 25:
+    print("You are a boomer.")
+else:
+    print("You are super cool.")
+
+
+if name == "Damien":
+    print("You are super cool.")
+elif age < 100:
+    print("You are probably still alive.")
+elif name == "David":
+    print("Hello David")
+
+    print("asdf")
+
+    thing: int = 4
+
+    print(thing * 8)
+else:
+    print("None of the conditions were met.")
+
+if name == "Damien":
+    print("You are super cool.")
+elif age < 100:
+    print("You are probably still alive.")
+
+if name == "Damien":
+    print("You are super cool.")
+
+if age < 100:
+    print("You are probably still alive.")
