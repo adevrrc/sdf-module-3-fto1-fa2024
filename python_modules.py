7116a85386c40d327fd8bc6154de46c3457b0dd8
Damien Altenburg <daltenburg@rrc.ca>
2024-09-26 13:58:53 -0500

    Update script to include examples of importing built-in Python modules math and random.

--- python_modules.py
+++ python_modules.py
@@ -4,5 +4,18 @@ Author:
 Date:
 Usage: python_modules.py
 """
+import math
+#import random
+from random import randint, choice
 
 teams = ["jets", "flames", "oilers", "canucks", "senators", "canadians"]
+
+number = 64
+
+print(math.sqrt(number))
+
+for count in range(5):
+    dice_roll = randint(1, 20)
+    print(dice_roll)
+
+print(f"My favorite hockey team is {choice(teams)}.")
