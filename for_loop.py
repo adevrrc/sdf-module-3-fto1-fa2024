2e615811f790d694fd61f3ca1dfafc8193bb0874
Damien Altenburg <daltenburg@rrc.ca>
2024-09-26 13:58:04 -0500

    Update script to include examples of the for loop.

--- for_loop.py
+++ for_loop.py
@@ -5,9 +5,15 @@ Date:
 Usage: python for_loop.py
 """
 
-THRESHOLD = 23
+THRESHOLD = 27
 temperatures = [19, 23, 24, 18, 24, 29, 35, 35, 23, 27]
 number_of_temperatures_below_threshold = 0
+
+for temperature in temperatures:
+    if temperature < THRESHOLD:
+        number_of_temperatures_below_threshold += 1
+
+print(number_of_temperatures_below_threshold)
 
 uppercase_letter_count = 0
 lowercase_letter_count = 0
@@ -18,3 +24,9 @@ hockey_teams = {
     "calgary": "flames",
     "edmonton": "oilers"
 }
+
+for key in hockey_teams:
+    print(f"{key.title()} {hockey_teams[key].title()}")
+
+for number in range(1, 11):
+    print(number)
