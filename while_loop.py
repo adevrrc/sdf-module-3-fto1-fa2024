5bfe0e76e8af01b25e72b5d84adb3fd10f6bca9b
Damien Altenburg <daltenburg@rrc.ca>
2024-09-26 13:57:32 -0500

    Update script to include an example of the while loop.

--- while_loop.py
+++ while_loop.py
@@ -4,3 +4,16 @@ Author:
 Date:
 Usage: python while_loop.py
 """
+new_password = "new_password"
+password_confirmation = "password_confirmation"
+
+while new_password != password_confirmation:
+    new_password = input("Enter the new password: ")
+
+    password_confirmation = input("Confirm the password: ")
+
+    # when the password is not confirmed
+    if new_password != password_confirmation:
+        print("Passwords did not match. Try again.")
+    else:
+        print("Password updated!")
